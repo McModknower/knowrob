da02af8c5566d918c0ccc30f06ed30a3da0fbe9f
McModknower <teetoll@t-online.de>
2023-06-21 10:57:00 +0200

    add position_distance to calculate the euclidian distance between two positions

--- src/reasoning/spatial/distance.pl
+++ src/reasoning/spatial/distance.pl
@@ -1,5 +1,6 @@
 :- module(spatial_distance,
-    [ object_distance(r,r,?)
+    [ object_distance(r,r,?),
+      position_distance(+,+,?)
     ]).
 
 % TODO: consider qualitative distance: close-to, far-away-from, ...
@@ -16,8 +17,18 @@ object_distance(A,B,Distance) :-
 	ground(A),
 	ground(B),
 	% FIXME: hardcoded map
-	is_at(A, [map,[AX,AY,AZ],_]),
-	is_at(B, [map,[BX,BY,BZ],_]),
+	is_at(A, [map,PosA,_]),
+	is_at(B, [map,PosB,_]),
+	position_distance(PosA, PosB, Distance).
+
+%% position_distance(+A:Pos, +B:Pos, ?Distance:float) is det.
+%
+% Computes euclidean distance between A and B.
+%
+% @param A         list of 3 floats
+% @param B         list of 3 floats
+% @param Distance  The current distance between A and B
+position_distance([AX,AY,AZ],[BX,BY,BZ],Distance) :-
 	DX is AX - BX,
 	DY is AY - BY,
 	DZ is AZ - BZ,
