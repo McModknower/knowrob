5e76efc3ed6df0698ea66542b0dbf8ed391928ee
Daniel Beßler <danielb@uni-bremen.de>
2024-06-22 14:16:22 +0200

    Added Python conversion rule for const QueryContext

--- src/queries/QueryContext.cpp
+++ src/queries/QueryContext.cpp
@@ -12,11 +12,16 @@ namespace knowrob::py {
 	template<>
 	void createType<QueryContext>() {
 		using namespace boost::python;
-		class_<QueryContext, std::shared_ptr<QueryContext>>
+		class_<QueryContext, std::shared_ptr<QueryContext>, boost::noncopyable>
 				("QueryContext", init<int>())
 				.def(init<const QueryContext &, const ModalOperatorPtr &>())
 				.def_readwrite("queryFlags", &QueryContext::queryFlags)
 				.def_readwrite("modalIteration", &QueryContext::modalIteration)
 				.def_readwrite("selector", &QueryContext::selector);
+
+		// QueryContextPtr uses `const QueryContext` which currently requires
+		// a custom converter to be defined in order to be used in Python.
+		register_ptr_to_python< std::shared_ptr< const QueryContext > >();
+		implicitly_convertible< std::shared_ptr< QueryContext >, std::shared_ptr< const QueryContext > >();
 	}
 }
