cf3519b9fd1f77997a809d0f202f1b621eeef441
Daniel Beßler <danielb@uni-bremen.de>
2024-08-19 16:59:09 +0200

    refactor

--- tests/py/dummy_reasoner.py
+++ tests/py/dummy_reasoner.py
@@ -6,16 +6,19 @@ class DummyReasoner(GoalDrivenReasoner):
 		#enableFeature(GoalDrivenReasonerFeature.SupportsSimpleConjunctions)
 		#enableFeature(GoalDrivenReasonerFeature.SupportsExtensionalGrounding)
 		self.defineRelation(PredicateIndicator("dummy", 2))
+		print("DummyReasoner()")
 
 	def setDataBackend(self, dataBackend):
-		print("setDataBackend: " + str(dataBackend))
+		print("setDataBackend: " + str(type(dataBackend)))
 		return None
 
 	def initializeReasoner(self, config):
-		print("initializeReasoner: " + str(config))
+		print("initializeReasoner: " + str(type(config)))
 		return True
 
 	def evaluateQuery(self, query):
-		print("evaluateQuery: " + str(query))
-		# print("evaluateQuery: " + str(query.formula().literals()))
+		formula = query.formula()
+		literal = formula.literals()[0]
+		predicate = literal.predicate()
+		print("evaluateQuery: " + str(predicate))
 		return None
