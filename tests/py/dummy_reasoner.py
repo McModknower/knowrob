bfce2488e473406cf6eaa017505b0f7de65467d3
Daniel Beßler <danielb@uni-bremen.de>
2024-08-20 19:48:22 +0200

    Made the Python reasoner example more interesting

--- tests/py/dummy_reasoner.py
+++ tests/py/dummy_reasoner.py
@@ -1,15 +1,16 @@
 from knowrob import *
 
+
 class DummyReasoner(GoalDrivenReasoner):
 	def __init__(self):
 		super(DummyReasoner, self).__init__()
-		#enableFeature(GoalDrivenReasonerFeature.SupportsSimpleConjunctions)
-		#enableFeature(GoalDrivenReasonerFeature.SupportsExtensionalGrounding)
-		self.defineRelation(PredicateIndicator("jealous", 2))
 		self.storage = None
+		self.loves = IRIAtom.Tabled("http://knowrob.org/kb/lpn#loves")
+		self.jealous = IRIAtom.Tabled("http://knowrob.org/kb/lpn#jealous")
+		self.defineRelation(PredicateIndicator(self.jealous.stringForm(), 2))
 
 	def setDataBackend(self, storage):
-		# keep a reference on the storage, as it is used in evaluateQuery
+		# keep a handle on the storage, as it is used in evaluateQuery
 		self.storage = storage
 
 	def initializeReasoner(self, config):
@@ -17,15 +18,31 @@ class DummyReasoner(GoalDrivenReasoner):
 		return True
 
 	def evaluateQuery(self, query):
-		formula = query.formula()
-		literal = formula.literals()[0]
+		literal = query.formula().literals()[0]
 		predicate = literal.predicate()
-		loves = 'http://knowrob.org/kb/lpn#loves'
-		x = predicate.arguments()[0]
-		y = predicate.arguments()[2]
-		z = Variable("z")
-		p1 = Predicate(loves, [x, z])
-		p2 = Predicate(loves, [y, z])
-		print("evaluateQuery: " + str(predicate) + " <-- " + str([p1, p2]))
-		gq = GraphSequence([GraphPattern(p1), GraphPattern(p2)])
-		return None
+		subj = predicate.arguments()[0]
+		obj = predicate.arguments()[2]
+		# create a query that checks if subj and obj both love the same person
+		query_term = GraphSequence([
+			GraphPattern(subj, self.loves, Variable("z")),
+			GraphPattern(obj, self.loves, Variable("z"))])
+		# execute the query using the storage of the reasoner and call handleSolution for each solution.
+		# a solution is represented as a dictionary of variable bindings that can be applied to the
+		# predicate to create a new instance.
+		self.storage.query(GraphQuery(query_term), lambda bindings: self.handleSolution(query, predicate, bindings))
+		return True
+
+	def handleSolution(self, query, predicate, bindings):
+		# create a new instance of the predicate with the bindings
+		instance = applyBindings(predicate, bindings)
+		# check if the subject and object are different
+		subj = instance.arguments()[0]
+		obj = instance.arguments()[2]
+		if subj == obj:
+			# skip self, cannot be done in graph query at the moment as no NOT_EQUAL
+			# operator is part of the query language.
+			return
+		# generate a new answer with the bindings, and add the instance as grounding
+		yes = AnswerYes(bindings)
+		yes.addGrounding(instance)
+		query.push(yes)
