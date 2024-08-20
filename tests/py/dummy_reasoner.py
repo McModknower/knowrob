30d6e4dc55c2ed9ed5974f275238737fc7a42d1a
Daniel Beßler <danielb@cs.uni-bremen.de>
2024-08-20 15:28:36 +0200

    work on making testcase more interesting

--- tests/py/dummy_reasoner.py
+++ tests/py/dummy_reasoner.py
@@ -5,20 +5,27 @@ class DummyReasoner(GoalDrivenReasoner):
 		super(DummyReasoner, self).__init__()
 		#enableFeature(GoalDrivenReasonerFeature.SupportsSimpleConjunctions)
 		#enableFeature(GoalDrivenReasonerFeature.SupportsExtensionalGrounding)
-		self.defineRelation(PredicateIndicator("dummy", 2))
-		print("DummyReasoner()")
+		self.defineRelation(PredicateIndicator("jealous", 2))
+		self.storage = None
 
-	def setDataBackend(self, dataBackend):
-		print("setDataBackend: " + str(type(dataBackend)))
-		return None
+	def setDataBackend(self, storage):
+		# keep a reference on the storage, as it is used in evaluateQuery
+		self.storage = storage
 
 	def initializeReasoner(self, config):
-		print("initializeReasoner: " + str(type(config)))
+		# nothing to do here
 		return True
 
 	def evaluateQuery(self, query):
 		formula = query.formula()
 		literal = formula.literals()[0]
 		predicate = literal.predicate()
-		print("evaluateQuery: " + str(predicate))
+		loves = 'http://knowrob.org/kb/lpn#loves'
+		x = predicate.arguments()[0]
+		y = predicate.arguments()[2]
+		z = Variable("z")
+		p1 = Predicate(loves, [x, z])
+		p2 = Predicate(loves, [y, z])
+		print("evaluateQuery: " + str(predicate) + " <-- " + str([p1, p2]))
+		gq = GraphSequence([GraphPattern(p1), GraphPattern(p2)])
 		return None
