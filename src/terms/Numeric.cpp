fa386e64c8bf625475f302095589542d18329db7
Daniel Beßler <danielb@uni-bremen.de>
2024-08-25 22:44:35 +0200

    simplified Python interface of Numeric: as* methods are not needed as Python can always use numericForm

--- src/terms/Numeric.cpp
+++ src/terms/Numeric.cpp
@@ -23,48 +23,13 @@ std::shared_ptr<Numeric> Numeric::falseAtom() {
 }
 
 namespace knowrob::py {
-	// this struct is needed because Numeric has pure virtual methods
-	struct NumericWrap : public Numeric, boost::python::wrapper<Numeric> {
-		explicit NumericWrap(PyObject *p, XSDType xsdType) : self(p), Numeric(xsdType) {}
-
-		double asDouble() const override { return call_method<double>(self, "asDouble"); }
-
-		float asFloat() const override { return call_method<float>(self, "asFloat"); }
-
-		int asInteger() const override { return call_method<int>(self, "asInteger"); }
-
-		long asLong() const override { return call_method<long>(self, "asLong"); }
-
-		short asShort() const override { return call_method<short>(self, "asShort"); }
-
-		unsigned int asUnsignedInteger() const override { return call_method<unsigned int>(self, "asUnsignedInteger"); }
-
-		unsigned long asUnsignedLong() const override { return call_method<unsigned long>(self, "asUnsignedLong"); }
-
-		unsigned short asUnsignedShort() const override { return call_method<unsigned short>(self, "asUnsignedShort"); }
-
-		bool asBoolean() const override { return call_method<bool>(self, "asBoolean"); }
-
-	private:
-		PyObject *self;
-	};
-
 	template<>
 	void createType<Numeric>() {
 		using namespace boost::python;
-		class_<Numeric, std::shared_ptr<NumericWrap>, bases<XSDAtomic>, boost::noncopyable>
+		class_<Numeric, std::shared_ptr<Numeric>, bases<XSDAtomic>, boost::noncopyable>
 				("Numeric", no_init)
 				.def("isFloatingNumber", &Numeric::isFloatingNumber)
-				.def("isSameNumeric", &Numeric::isSameNumeric)
-				.def("asDouble", pure_virtual(&Numeric::asDouble))
-				.def("asFloat", pure_virtual(&Numeric::asFloat))
-				.def("asInteger", pure_virtual(&Numeric::asInteger))
-				.def("asLong", pure_virtual(&Numeric::asLong))
-				.def("asShort", pure_virtual(&Numeric::asShort))
-				.def("asUnsignedInteger", pure_virtual(&Numeric::asUnsignedInteger))
-				.def("asUnsignedLong", pure_virtual(&Numeric::asUnsignedLong))
-				.def("asUnsignedShort", pure_virtual(&Numeric::asUnsignedShort))
-				.def("asBoolean", pure_virtual(&Numeric::asBoolean));
+				.def("isSameNumeric", &Numeric::isSameNumeric);
 		class_<Double, std::shared_ptr<Double>, bases<Numeric>>
 				("Double", init<double>())
 				.def(init<std::string_view>())
