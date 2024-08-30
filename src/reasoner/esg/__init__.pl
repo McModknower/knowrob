973eeae0efe80b0c470535d232ef119a8a25938e
Daniel Beßler <danielb@uni-bremen.de>
2024-08-30 12:58:50 +0200

    Register computables for interval predicates

--- src/reasoner/esg/__init__.pl
+++ src/reasoner/esg/__init__.pl
@@ -4,6 +4,19 @@
 :- sw_register_prefix(knowrob, 'http://knowrob.org/kb/knowrob.owl#').
 
 :- use_module('esg').
+
 :- use_module('interval').
+:- sw_register_computable(soma:simultaneous,	interval_equals).
+:- sw_register_computable(soma:before,			interval_before).
+:- sw_register_computable(soma:after,			interval_after).
+:- sw_register_computable(soma:meets,			interval_meets).
+:- sw_register_computable(soma:metBy,			interval_met_by).
+:- sw_register_computable(soma:starts,			interval_starts).
+:- sw_register_computable(soma:startedBy,		interval_started_by).
+:- sw_register_computable(soma:finishes,		interval_finishes).
+:- sw_register_computable(soma:overlappedOn,	interval_overlaps).
+:- sw_register_computable(soma:overlappedBy,	interval_overlapped_by).
+:- sw_register_computable(soma:during,			interval_during).
+
 :- use_module('workflow').
 :- use_module('parser').
